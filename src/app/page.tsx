063adfd0c2c099a4745f22ac2f39f9fcf95f0f0e
Esteban Dib <esteban@ungapp.com>
2023-07-10 22:39:44 -0400

    fix(home): center buttons and add message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,30 @@
 "use client";
 
 import Link from 'next/link'
-import { Button, Stack } from '@mui/material'
+import { Button, Container, Stack, Typography } from '@mui/material'
 
 export default function Home() {
   return (
-    <main>
-      <Stack direction="row" spacing={2}>
-      <Link href="/buy">
-        <Button
-          variant="outlined"
-          color="primary"
-        >
-          I want to buy
-        </Button>
-      </Link>
-      <Link href="/sell">
-        <Button
-          variant="outlined"
-          color="secondary"
-        >
-          I want to sell
-        </Button>
-      </Link>
+    <Container maxWidth="xl" sx={{ pt: '15%' }}>
+        <Typography variant="h4" mb={4} textAlign="center">What do you want to do?</Typography>
+        <Stack direction="row" spacing={2} justifyContent="center">
+          <Link href="/buy">
+            <Button
+              variant="outlined"
+              color="primary"
+            >
+              I want to buy
+            </Button>
+          </Link>
+          <Link href="/sell">
+            <Button
+              variant="outlined"
+              color="secondary"
+            >
+              I want to sell
+            </Button>
+          </Link>
       </Stack>
-    </main>
+    </Container>
   )
 }
